a9d50a3e746c238918fc9ababef995eec6b40fe0
Artem Pechenkin <artfeels.production@gmail.com>
2019-07-17 20:33:03 -0500

    initial commit

---
+++ src/main/java/com/example/reminder2/Reminder2Application.java
@@ -0,0 +1,13 @@
+package com.example.reminder2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Reminder2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Reminder2Application.class, args);
+    }
+
+}
