1c445bb20c9ffca6797869cbca5288f1efd81b4d
Art Bundy <artem.s.pechenkin@gmail.com>
2019-07-17 20:51:26 -0500

    my new feature 1

--- src/main/java/com/example/reminder2/controllers/ReminderController.java
+++ src/main/java/com/example/reminder2/controllers/ReminderController.java
@@ -2,5 +2,10 @@ package com.example.reminder2.controllers;
 
 public class ReminderController {
     public String a;
+
+    @Override
+    public int hashCode() {
+        return super.hashCode();
+    }
 }
 
