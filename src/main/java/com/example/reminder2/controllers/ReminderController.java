3b7e5800a4ea2ee43070a2326a1506163603e78a
Art Bundy <artem.s.pechenkin@gmail.com>
2019-07-17 20:59:57 -0500

    Something refactored

--- src/main/java/com/example/reminder2/controllers/ReminderController.java
+++ src/main/java/com/example/reminder2/controllers/ReminderController.java
@@ -2,10 +2,5 @@ package com.example.reminder2.controllers;
 
 public class ReminderController {
     public String a;
-
-    @Override
-    public int hashCode() {
-        return super.hashCode();
-    }
 }
 
